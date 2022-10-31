a41431f3634dd9c1c55ff0f5674e80a4d1063119
megta <113287791+megta@users.noreply.github.com>
2022-10-31 17:20:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const ratingcard = document.querySelector('.ratingcard');
+const thankcard = document.querySelector('.thankcard');
+const submit = document.querySelector('.ratingcard__submit');
+
+
+thankcard.hidden = true;
+
+function thanks() {
+    if (submit.clicked) {
+    ratingcard.hidden = true;
+    thankcard.hidden= false;
+    }
+}
+
+submit.addEventListener('click', thanks);
