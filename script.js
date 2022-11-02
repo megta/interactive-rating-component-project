8390e47e645205b88c097b7c47bb5a9bc6190526
megta <113287791+megta@users.noreply.github.com>
2022-11-02 16:10:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
 'use strict';
 
-/*
 const ratingCard = document.querySelector('.ratingcard');
 const thankCard = document.querySelector('.thankcard');
-const form = document.querySelector('form');
-const submitBtn = document.querySelector('.ratingcard__submit'); //not sure to use it
-
-function hideCard() {
-if(thankCard.classList.contains('.ratingcard__submit')) {
-    thankCard.classList.remove('.ratingcard__submit');
-}
-}
-
-*/
+// const submitBtn = document.querySelector('.ratingcard__submit'); not sure to use it
 
 
-/* if cannot figure out how to use database then change button type to type=button
-instead of type=submit */
+ratingCard.addEventListener('submit', (e) => {
+    e.preventDefault();
+    thankCard.style.display = block;
 
-// to use form data, can try label = .thankcard__number.value to get the rating num
-
-/*
-submitBtn.addEventListener('click', hideCard);
-form.addEventListener('submit', (event) => {
-event.preventDefault();
-
-const ratingOption = document.querySelector('.ratingcard__btn').value;
-let ratingNum = document.querySelector('.thankcard__number');
-
+    const ratingOption = document.querySelector('input:checked').value;
+    let ratingNum = document.querySelector('.thankcard__number') + ratingOption;
 
 });
-*/
+
+
+
+
